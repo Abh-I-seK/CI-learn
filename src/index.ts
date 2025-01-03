e4dc50065143fce950aa408ce26c59b027d3e198
Abhisek <134422863+Abh-I-seK@users.noreply.github.com>
2025-01-03 21:00:41 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello World !!");
 })
 
+app.get("/product", (req, res) => {
+  res.send("This is Product Page");
+})
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
 })
